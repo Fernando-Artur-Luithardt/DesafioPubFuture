cb5c8b601c13867d364d47cc1c7d405018e7d062
Fernando-Artur-Luithardt <fluithardt@gmail.com>
2022-01-11 20:58:51 -0300

    endPoint usuario/deletar

---
+++ usuario/deletar/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require('./../../auth.php'); //verifica se está logado
+require('./../../banco.php');
+
+$id = $_SESSION['usuario']['id'];
+
+$sql = "DELETE from `usuario` WHERE id = $id";
+
+if (mysqli_query($conn,$sql)) {
+    http_response_code(204);
+    session_destroy();
+    exit;
+}
+
+$response = array('mensagem' => "Erro ao deletar usuario");
+$responseJson = json_encode($response);
+http_response_code(400);
+echo $responseJson;
+exit;
+
+?>
