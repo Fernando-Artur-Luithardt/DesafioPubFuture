2f1ea6559debb50e20d4aa5430e48a5bfe3c5e05
Fernando-Artur-Luithardt <fluithardt@gmail.com>
2022-01-11 20:20:22 -0300

    verificação de erros no banco

--- banco.php
+++ banco.php
@@ -7,5 +7,12 @@
     $base = "fernando_pubfuture"; // qual a base de dados
     
     $conn = mysqli_connect($host, $user, $pass, $base);
-      
+    
+    if (mysqli_connect_errno()) {
+        $response = array('mensagem' => mysqli_connect_error());
+        $responseJson = json_encode($response);
+        http_response_code(400);
+        echo $responseJson;
+        exit();
+    }
 ?>
