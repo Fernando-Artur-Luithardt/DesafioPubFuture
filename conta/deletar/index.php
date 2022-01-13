c7c48b6fadb8e20b09c0c7c27fb91163a9d2a28e
Fernando-Artur-Luithardt <fluithardt@gmail.com>
2022-01-12 22:01:40 -0300

    endPoint conta/deletar funcionando

---
+++ conta/deletar/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require('./../../auth.php'); //verifica se está logado
+require('./../../banco.php');
+
+$id = $_SESSION['usuario']['id'];
+$codConta = isset($_POST['codConta'])? $_POST['codConta']: "";
+
+$sql = "DELETE from `conta` WHERE userId = $id AND codConta = $codConta";
+
+if (mysqli_query($conn,$sql)) {
+    http_response_code(200);
+    exit;
+}
+
+$response = array('mensagem' => "Erro ao deletar conta");
+$responseJson = json_encode($response);
+http_response_code(400);
+echo $responseJson;
+exit;
+
+?>
