1ebd86c217f156ff7487553bec493f3ea855517d
Fernando-Artur-Luithardt <fluithardt@gmail.com>
2022-01-14 11:44:49 -0300

    trocando dados com servidos via jquery OK

--- index.php
+++ index.php
@@ -28,11 +28,33 @@ require 'banco.php';
         </div>
 
         <div>
-            <button>login</button>
+            <button onclick="chamar()">login</button>
         </div>
-
-
     </div>
-    
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+<script>
+
+    function chamar(){
+        let dados = new FormData();
+
+dados.append('usuario', 'fedsadarnando');
+dados.append('senha', '1234');
+
+$.ajax({
+    type: "POST",
+    url: 'http://localhost/DesafioPubFuture/usuario/cadastrar/',
+    data: dados,
+    processData: false,
+    contentType: false,
+    success: function(resposta){
+    window.location.replace('http://localhost/desafioPubFuture/controle.php');
+    console.log(resposta)
+    }
+})  }
+
+
+</script>
 </body>
 </html>
