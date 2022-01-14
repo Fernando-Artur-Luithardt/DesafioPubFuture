c39358c090de6ea71f838d29885929f103c45654
Fernando-Artur-Luithardt <fluithardt@gmail.com>
2022-01-14 18:16:53 -0300

    recebendo o json das contas do usuario logado OK

--- controle.php
+++ controle.php
@@ -1,3 +1,9 @@
+<?php
+require('auth.php');
+require('banco.php');
+require('constantes.php');
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -6,7 +12,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>controleFinanceiro</title>
 </head>
-<body>
-    
+<body> 
+<h1></h1>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" defer></script>
+<script defer>
+
+fetch('http://localhost/DesafioPubFuture/conta/listar/')
+.then(function(respostaDoServidor) {
+	return respostaDoServidor.json()
+})
+.then(function(dadosContas) {
+    console.log(dadosContas)
+})
+</script>
 </body>
 </html>
