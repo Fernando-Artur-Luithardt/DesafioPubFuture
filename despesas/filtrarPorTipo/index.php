aeb776d409602a8c4de1f0c8a3a0a764cbed3ec1
Fernando-Artur-Luithardt <fluithardt@gmail.com>
2022-01-16 16:57:00 -0300

    despesas/filtrarPorTipo v1.0

---
+++ despesas/filtrarPorTipo/index.php
@@ -0,0 +1,39 @@
+<?php
+
+require('./../../auth.php');
+require('./../../banco.php');
+require('./../../constantes.php');
+require('./../../contaVsUsuario.php');
+
+$despesasArr = array();
+$userId = $_SESSION['usuario']['id'];
+$codConta = isset($_POST['codConta'])? $_POST['codConta']: "";
+$categoriaDespesa = isset($_POST['categoria'])? $_POST['categoria']: "";
+
+if(empty($codConta) || empty($categoriaDespesa)) {
+    $response = array('mensagem' => "necessário codConta e categoriadespesa para filtrar despesas");
+    $responseJson = json_encode($response);
+    http_response_code(400);
+    echo $responseJson;
+    exit;
+}
+
+$sql = "SELECT id, categoria, codConta, dataEntrada, dataPrevista, descricao, valor, ativo FROM `despesas` WHERE codConta = $codConta AND categoria = $categoriaDespesa";   
+$consultaDespesas = mysqli_query($conn,$sql);
+
+while ($despesas = mysqli_fetch_array($consultaDespesas)) {
+    $despesasArr[] = [
+        'id' => $despesas['id'],
+        'categoria' => $tiposDespesas[$despesas['categoria']],
+        'codConta' => $despesas['codConta'],
+        'dataEntrada' => $despesas['dataEntrada'],
+        'dataPrevista' => $despesas['dataPrevista'],
+        'descricao' => $despesas['descricao'],
+        'valor' => $despesas['valor'],
+        'ativo' => $despesas['ativo'],
+    ];
+}
+
+echo json_encode($despesasArr);
+
+?>
