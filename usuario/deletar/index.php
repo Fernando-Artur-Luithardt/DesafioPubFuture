6bd28ac04c4b46dec7f0e028db17a1c5eef7e425
Fernando-Artur-Luithardt <fluithardt@gmail.com>
2022-01-15 22:45:46 -0300

    usuario/deletar exige POST deletar: 1 para segurança

--- usuario/deletar/index.php
+++ usuario/deletar/index.php
@@ -6,13 +6,15 @@ require('./../../banco.php');
 $id = $_SESSION['usuario']['id'];
 
 $sql = "DELETE from `usuario` WHERE id = $id";
+$deletar = isset($_POST['deletar'])? $_POST['deletar']: "";
 
-if (mysqli_query($conn,$sql)) {
-    http_response_code(204);
-    session_destroy();
-    exit;
+if($deletar == 1) {
+    if (mysqli_query($conn,$sql)) {
+        http_response_code(204);
+        session_destroy();
+        exit;
+    }
 }
-
 $response = array('mensagem' => "Erro ao deletar usuario");
 $responseJson = json_encode($response);
 http_response_code(400);
